d26c22595d96e8047990f994d744656541dce46a
Jane-Kotovich <jenyakotovich@gmail.com>
2022-02-22 15:05:25 +1000

    Rendering project list

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,36 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
   };
   return adjDescriptor;
 }
+// projectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+  constructor(private type: "active" | "finished") {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-list")!
+    );
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-project`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
 
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -131,3 +161,5 @@ class ProjectInput {
 }
 
 const prInput = new ProjectInput();
+const activePrList = new ProjectList("active");
+const finishedPrList = new ProjectList("finished");
